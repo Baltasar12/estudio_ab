5ae5659f3d6ef0a3c3e851495fbeadb892110aad
Baltasar Barbaglia <baltasarbarbaglia@gmail.com>
2023-05-02 22:27:05 -0300

    Commit 5:

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/* 
 function update() {
     var element = document.getElementById("myprogressBar");   
     var width = 1;
@@ -11,4 +11,55 @@ function update() {
         element.style.width = width + '%'; 
       }
     }
+  } */
+
+  const progressBar = document.querySelector(".progress");
+let progressValue = 0;
+
+setInterval(() => {
+  if (progressValue < 100) {
+    progressValue += 10;
+    progressBar.style.width = `${progressValue}%`;
   }
+}, 1000);
+
+
+
+var xValues = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("chart-dia", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Oficios contestados por días"
+    }
+  }
+});
+new Chart("chart-persona", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Oficios contestados por persona"
+    }
+  }
+});
