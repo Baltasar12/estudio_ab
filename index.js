b6d38d5eae06d5d8c7485268b21e006b759aa545
Baltasar Barbaglia <baltasarbarbaglia@gmail.com>
2023-05-29 18:31:58 -0300

    Commit 17: más correcciones

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function update() {
     }
   } */
 
-  const progressBar = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress");
 let progressValue = 0;
 
 setInterval(() => {
@@ -28,6 +28,15 @@ setInterval(() => {
 var xValues = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
 var yValues = [55, 49, 44, 24, 15];
 var barColors = ["#074073", "#074073","#074073","#074073","#074073"];
+var chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(231,233,237)'
+} 
 
 new Chart("chart-dia", {
   type: "bar",
@@ -43,6 +52,18 @@ new Chart("chart-dia", {
     title: {
       display: true,
       text: "Oficios contestados por días"
+    },
+    scales:{
+      y:{
+        grid:{
+          color: chartColors.grey
+        }
+      },
+      x:{
+        grid:{
+          color: chartColors.grey
+        }
+      }
     }
   }
 });
@@ -60,6 +81,18 @@ new Chart("chart-persona", {
     title: {
       display: true,
       text: "Oficios contestados por persona"
+    },
+    scales:{
+      y:{
+        grid:{
+          color: chartColors.grey
+        }
+      },
+      x:{
+        grid:{
+          color: chartColors.grey
+        }
+      }
     }
   }
 });
